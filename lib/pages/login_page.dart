cc9e4cb7d87d2f4342d6d5e6febbba45a06ad1c1
MiljenkoPetrovic <80465450+MiljenkoPetrovic@users.noreply.github.com>
2023-09-12 15:29:53 +0200

    Login UI complete

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -15,7 +15,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
+      backgroundColor: Colors.blue[100],
       body: SafeArea(
         child: Center(
           child: Column(
