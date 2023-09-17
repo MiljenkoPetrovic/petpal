ac64bd72a7e51de82e10261d1b9c7e091de8ccea
MiljenkoPetrovic <80465450+MiljenkoPetrovic@users.noreply.github.com>
2023-09-17 04:35:46 +0200

    removed the navbart widget because it wasn't needed. Updated home.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,30 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:petpal/pages/login_or_register.dart';
-import 'package:petpal/components/navbar.dart'; // Import NavbarPage
+import 'package:petpal/pages/alerts.dart';
+import 'package:petpal/pages/veterinarians.dart';
+import 'package:petpal/pages/tracker.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  // Define a list of page widgets for navigation
+  final List<Widget> _pages = [
+    HomeContent(),
+    TrackerPage(
+      storage: FirebaseStorage.instance,
+    ),
+    AlertsPage(),
+    Veterinarians(),
+  ];
 
   void signUserOut(BuildContext context) async {
     await FirebaseAuth.instance.signOut();
@@ -28,13 +48,52 @@ class HomePage extends StatelessWidget {
         ],
         title: Text("Home"),
       ),
-      body: Center(
-        child: Text(
-          "Welcome, ${user?.email ?? 'Guest'}!",
-          style: TextStyle(fontSize: 24),
-        ),
+      body: _pages[_currentIndex], // Ensure _currentIndex is within valid range
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color.fromARGB(255, 2, 80, 110),
+        fixedColor: Colors.lightBlueAccent,
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+
+          // No need for Navigator on the web, just update the index
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment_outlined),
+            label: 'Tracker',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_alert),
+            label: 'Alerts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_location),
+            label: 'Veterinarias',
+          ),
+        ],
       ),
-      bottomNavigationBar: NavbarPage(), 
     );
   }
 }
+
+class HomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final User? user = FirebaseAuth.instance.currentUser;
+    return Center(
+      child: Text(
+        "Welcome, ${user?.email ?? 'Guest'}!",
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
+
+// Define other pages similarly to TrackerPage, AlertsPage, and VeterinariansPage
